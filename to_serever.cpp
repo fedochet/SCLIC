0957a6e0732764faa10eda67ee3c2cafb43901b8
Roman <fedochet@yandex.ru>
2014-11-16 03:51:17 +0400

    Artemy succeed

---
+++ to_serever.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+// включаем файлы, необходимые для tcp и сокетов
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h> // inet_pton()
+#include <errno.h>
+
+#define SERVER "149.154.167.40" // telegram test-server ip
+#define PORT 443 // port
+
+using std::cout;
+using std::endl;
+
+// немного спиздил у чмыря
+bool connect(const char *host, int port)
+{
+    int fd = socket (AF_INET, SOCK_STREAM, 0);
+    if (fd == -1)
+        return 0;
+
+    int flags = -1;
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons (port);
+    inet_pton(AF_INET,host, &addr.sin_addr.s_addr);
+
+    if (connect (fd, (struct sockaddr *) &addr, sizeof (addr)) == -1)
+    {
+        if (errno != EINPROGRESS)
+        {
+            close (fd);
+            return 0;
+        }
+    }
+
+
+    char *buffer = "netinet";
+    cout<<strlen(buffer)<<endl;
+
+
+    int n = write(fd,buffer,strlen(buffer));
+    cout<<buffer<<" "<<endl;
+    cout<<read(fd,buffer,strlen(buffer))<<endl;
+
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    cout << connect(SERVER, PORT)<<endl;
+
+    return 0;
+}
